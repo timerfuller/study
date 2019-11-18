ac21cd240f13fe716ceea134a62b9edd09dd8636
wenxue <wenxue-1104@163.com>
2019-11-18 21:57:21 +0800

    hystrix dashboard代码提交

--- weifuwu/consumer1-feign/src/main/java/consumer1/ConfigBean.java
+++ weifuwu/consumer1-feign/src/main/java/consumer1/ConfigBean.java
@@ -1,8 +1,10 @@
 package consumer1;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import com.netflix.loadbalancer.AvailabilityFilteringRule;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RoundRobinRule;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,5 +23,22 @@ public class ConfigBean {
     public IRule getRule(){
 //        return  new RoundRobinRule();  RandomRule AvailabilityFilteringRule WeightedResponseTimeRule RetryRule BestAvailableRule ZoneAvoidanceRule
         return new RoundRobinRule();
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+
+        registrationBean.setLoadOnStartup(1);
+
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+
+        registrationBean.setName("HystrixMetricsStreamServlet");
+
+
+        return registrationBean;
     }
 }
